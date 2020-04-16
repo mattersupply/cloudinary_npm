1ff26e09e685d3105984e344f8345423af87bf3c
Miguel Santamaría <miguel@mattersupply.co>
2020-04-16 16:41:43 -0500

    debugging

--- cloudinary.js
+++ cloudinary.js
@@ -1,1 +1,5 @@
+console.log('CLOUDINARY ENTRY POINT')
+console.log(process)
+console.log(process.versions)
+
 module.exports = Number(process.versions.node.split('.')[0]) < 8 ? require('./lib-es5/cloudinary') : require('./lib/cloudinary');
